7f77c053f214ee3ebb263a23f080dbf5952d7d37
aebinessa <abinessa0004@sdsu.edu>
2023-03-31 09:55:51 -0700

    expands page transitions func, error though

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ function PageTransitions(){
             sectBtns.forEach((btn) =>{
                 btn.classList.remove('active')
             })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById (id);
+            element.classList.add('active');
         }
     })
 }
